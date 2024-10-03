0ef2d40908356ce40ea53bceb6bd2122105925a6
gabri <gabriel14fev@gmail.com>
2024-10-03 09:00:34 -0300

    Migrations

---
+++ src/ProjectManager.Infrastructure.SQLServer/Migrations/20241003120011_AddMoreColumns.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectManager.Infrastructure.SQLServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoreColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "ProjectTasks",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Projects",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TargetDate",
+                table: "Projects",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "ProjectTasks");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "TargetDate",
+                table: "Projects");
+        }
+    }
+}
